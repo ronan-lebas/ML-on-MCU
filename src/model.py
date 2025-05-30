e1b9d236a5d29b1835dc6625a316e9003283b599
ronan-lebas <80069231+ronan-lebas@users.noreply.github.com>
2025-05-30 19:05:21 +0200

    improved model capacity

--- src/model.py
+++ src/model.py
@@ -5,25 +5,44 @@ import torch.nn.functional as F
 class AudioCNN(nn.Module):
     def __init__(self, num_classes, in_channels=1):
         super(AudioCNN, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, 16, kernel_size=3, stride=1, padding=1)  # -> [B, 16, F, T]
-        self.bn1 = nn.BatchNorm2d(16)
-        self.pool1 = nn.MaxPool2d(kernel_size=2)  # -> [B, 16, F/2, T/2]
+        
+        # Conv block 1
+        self.conv1 = nn.Conv2d(in_channels, 32, kernel_size=3, padding=1)  # [B, 32, F, T]
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=3, padding=1)
+        self.pool1 = nn.MaxPool2d(kernel_size=2)  # [B, 32, F/2, T/2]
+        
+        # Conv block 2
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.conv4 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        self.pool2 = nn.MaxPool2d(kernel_size=2)  # [B, 64, F/4, T/4]
+        
+        # Conv block 3
+        self.conv5 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.conv6 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
+        self.pool3 = nn.AdaptiveAvgPool2d((1, 1))  # [B, 128, 1, 1]
 
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
-        self.bn2 = nn.BatchNorm2d(32)
-        self.pool2 = nn.MaxPool2d(kernel_size=2)  # -> [B, 32, F/4, T/4]
-
-        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
-        self.bn3 = nn.BatchNorm2d(64)
-        self.pool3 = nn.AdaptiveAvgPool2d((1, 1))  # -> [B, 64, 1, 1]
-
-        self.fc = nn.Linear(64, num_classes)
-
+        # Fully connected
+        self.fc1 = nn.Linear(128, 64)
+        self.fc2 = nn.Linear(64, num_classes)
 
     def forward(self, x):
-        x = self.pool1(F.relu(self.bn1(self.conv1(x))))
-        x = self.pool2(F.relu(self.bn2(self.conv2(x))))
-        x = self.pool3(F.relu(self.bn3(self.conv3(x))))
+        # Block 1
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = self.pool1(x)
+        
+        # Block 2
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        x = self.pool2(x)
+        
+        # Block 3
+        x = F.relu(self.conv5(x))
+        x = F.relu(self.conv6(x))
+        x = self.pool3(x)
+        
+        # Classifier
         x = x.view(x.size(0), -1)
-        x = self.fc(x)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
         return x
