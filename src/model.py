ee105f17d22e99531c9c6c2c0c5d683a6f1050a0
ronan-lebas <80069231+ronan-lebas@users.noreply.github.com>
2025-06-11 12:15:16 +0200

    added new architecture

--- src/model.py
+++ src/model.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+### === Baseline model === ###
 class AudioCNN(nn.Module):
     def __init__(self, num_classes, in_channels=1):
         super(AudioCNN, self).__init__()
@@ -53,7 +54,7 @@ class AudioCNN(nn.Module):
         x = self.fc2(x)
         return x
 
-
+### === Small model === ###
 # class AudioCNN(nn.Module):
 #     def __init__(self, num_classes, in_channels=1):
 #         super(AudioCNN, self).__init__()
@@ -82,7 +83,45 @@ class AudioCNN(nn.Module):
 #         x = self.fc1(x)
 #         return x
 
+### === Small-mid model === ###
+# class AudioCNN(nn.Module):
+#     def __init__(self, num_classes, in_channels=1):
+#         super(AudioCNN, self).__init__()
 
+#         # Conv block 1
+#         self.conv1 = nn.Conv2d(in_channels, 32, kernel_size=3, padding=1)
+#         self.bn1 = nn.BatchNorm2d(32)
+#         self.conv2 = nn.Conv2d(32, 32, kernel_size=3, padding=1)
+#         self.bn2 = nn.BatchNorm2d(32)
+#         self.pool1 = nn.MaxPool2d(kernel_size=2)
+
+#         # Conv block 2
+#         self.conv3 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+#         self.bn3 = nn.BatchNorm2d(64)
+#         self.conv4 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+#         self.bn4 = nn.BatchNorm2d(64)
+#         self.pool2 = nn.AdaptiveAvgPool2d((1, 1))
+
+#         # Fully connected
+#         self.fc1 = nn.Linear(64, 32)
+#         self.fc2 = nn.Linear(32, num_classes)
+
+#     def forward(self, x):
+#         x = F.relu(self.bn1(self.conv1(x)))
+#         x = F.relu(self.bn2(self.conv2(x)))
+#         x = self.pool1(x)
+
+#         x = F.relu(self.bn3(self.conv3(x)))
+#         x = F.relu(self.bn4(self.conv4(x)))
+#         x = self.pool2(x)
+
+#         x = x.view(x.size(0), -1)
+#         x = self.fc1(x)
+#         x = F.relu(x)
+#         x = self.fc2(x)
+#         return x
+
+### === Very large model === ### 
 # class AudioCNN(nn.Module):
 #     def __init__(self, num_classes, in_channels=1):
 #         super(AudioCNN, self).__init__()
@@ -143,7 +182,7 @@ class AudioCNN(nn.Module):
 #         x = self.fc3(x)
 #         return x
 
-
+### === Large model === ###
 # class AudioCNN(nn.Module):
 #     def __init__(self, num_classes, in_channels=1):
 #         super(AudioCNN, self).__init__()
